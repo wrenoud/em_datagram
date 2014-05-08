8dac2b26d80936b92cbf43bf0ab2f14ed343f046
Weston Renoud <wrenoud@gmail.com>
2014-05-08 18:30:50 -0300

    closes #3

--- em3000_surf_ss_packet.py
+++ em3000_surf_ss_packet.py
@@ -1,4 +1,27 @@
 from structObject import *
 from em_datagram import em_datagram
 
-class em3000_surf_ss_packet: pass
+class EM3000_SURF_SS_RECORD(structObject):
+    _byte_order = little_endian
+    _field_order = (
+        "time",
+        "ss")
+    time=ctype_ushort(doc="Time in seconds since record start")
+    ss=ctype_ushort(
+        getter=lambda x: x/10.0,
+        doc="Sound speed (in dm/s) (incl. offset)")
+
+class EM3000_SURF_SS_BODY(structObject):
+    _byte_order = little_endian
+    _field_order = (
+        "num_records",
+        "records",
+        "spare")
+    num_records=ctype_ushort(doc="Number of entries")
+    records=struct_array(
+        object_type=EM3000_SURF_SS_RECORD,
+        len=lambda self: self.num_records)
+    spare=ctype_uchar()
+    
+class em3000_surf_ss_packet(em_datagram):
+    body=EM3000_SURF_SS_BODY
