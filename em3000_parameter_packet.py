bbfe3977792b795baa3c6a724a68a22d5c4a08ce
Weston Renoud <wrenoud@gmail.com>
2014-05-08 18:55:37 -0300

    added parsing of parameter string to __init__

--- em3000_parameter_packet.py
+++ em3000_parameter_packet.py
@@ -5,11 +5,22 @@ class EM3000_INSTALLATION_BODY(structObject):
     _byte_order=little_endian
     _field_order=(
         "second_serial",
-        "parameters")
+        "parameter_string")
+    __slots__ = ("parameters",)
     second_serial=ctype_ushort()
-    parameters=struct_array(
+    parameter_string=struct_array(
         object_type=ctype_char(),
         len=lambda self: len(self._bindata) - 5)
 
 class em3000_parameter_packet(em_datagram):
     body=EM3000_INSTALLATION_BODY
+    
+    def __init__(self, *args, **kargs):
+        super(em3000_parameter_packet,self).__init__(*args,**kargs)
+
+        _string = "".join(self.body.parameter_string[:])
+        if _string[-1] == '\x00':
+            _string = _string[:-2] # trim trailing comma and pad byte
+        else:
+            _string = _string[:-1] # trim trailing comma
+        self.body.parameters = dict([_param.split('=') for _param in  _string.split(',')])
