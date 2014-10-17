90e2a1096eef3f54801f96319f2772813eef6d74
Weston Renoud <renoud@qps.nl>
2014-10-17 12:03:08 -0700

    added height packet

---
+++ em3000_height_packet.py
@@ -0,0 +1,16 @@
+from structObject import *
+from em_datagram import em_datagram
+
+class EM3000_HEIGHT_BODY(structObject):
+    _byte_order=little_endian
+    _field_order=(
+        "height",
+        "type")
+    #__slots__ = []
+    height=ctype_int(
+    	doc="Height value",
+    	getter=lambda x: x/100.0)
+    type=ctype_char()
+
+class em3000_height_packet(em_datagram):
+    body=EM3000_HEIGHT_BODY
